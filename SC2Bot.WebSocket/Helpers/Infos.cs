a2ae537440eeb3f2ed4cecbd7e5c7ba506c1d422
aliso <aliso@CRUNCHY>
2017-05-27 13:37:08 +0200

    New Project discord.net 1

---
+++ SC2Bot.WebSocket/Helpers/Infos.cs
@@ -0,0 +1,30 @@
+﻿using AligulacSC2;
+using System;
+
+namespace SC2Bot.WebSocket.Helpers
+{
+    public class Infos
+    {
+        private static string _botApi = string.Empty;
+        public static string BotAPI
+        {
+            get
+            {
+                if (_botApi == string.Empty)
+                    _botApi = Environment.GetEnvironmentVariable("APIBotSC2");
+                return _botApi;
+            }
+        }
+
+        private static Aligulac _aligulac;
+        public static Aligulac Aligulac
+        {
+            get
+            {
+                if (_aligulac == null)
+                    _aligulac = new Aligulac(Environment.GetEnvironmentVariable("APIBotAligulacSC2"));
+                return _aligulac;
+            }
+        }
+    }
+}
---
+++ SC2Bot.WebSocket/Program.cs
@@ -0,0 +1,69 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace SC2Bot.WebSocket
+{
+    public class Program
+    {
+        private CommandService commands;
+        private DiscordSocketClient client;
+
+        public static void Main(string[] args)
+            => new Program().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
+        {
+            client = new DiscordSocketClient();
+            commands = new CommandService();
+
+            client.Log += Log;
+
+            await InstallCommands();
+
+            await client.LoginAsync(TokenType.Bot, Helpers.Infos.BotAPI);
+            await client.StartAsync();
+            
+            client.Ready += () =>
+            {
+                Console.WriteLine("Yolo swagghetti");
+                return Task.CompletedTask;
+            };
+
+            await Task.Delay(-1);            
+        }
+        public async Task InstallCommands()
+        {
+            client.MessageReceived += HandleCommand;
+            await commands.AddModulesAsync(Assembly.GetEntryAssembly());
+        }
+        public async Task HandleCommand(SocketMessage messageParam)
+        {
+            // Don't process the command if it was a System Message
+            var message = messageParam as SocketUserMessage;
+            if (message == null) return;
+
+            // Create a number to track where the prefix ends and the command begins
+            int argPos = 0;
+            // Determine if the message is a command, based on if it starts with '!' or a mention prefix
+            if (!(message.HasCharPrefix('!', ref argPos) || message.HasMentionPrefix(client.CurrentUser, ref argPos)))
+                return;
+            // Create a Command Context
+            var context = new CommandContext(client, message);
+            // Execute the command. (result does not indicate a return value, 
+            // rather an object stating if the command executed succesfully)
+            var result = await commands.ExecuteAsync(context, argPos);
+            if (!result.IsSuccess)
+                await context.Channel.SendMessageAsync(result.ErrorReason);
+        }
+        
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine($"[{msg.Severity}] {msg.Source}: {msg.Message}");
+            return Task.CompletedTask;
+        }
+    }
+}
