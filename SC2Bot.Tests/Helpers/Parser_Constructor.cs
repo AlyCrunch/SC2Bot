8deab9e478b6c10d0dea46d133c1e782ea8bbbd6
godu <arthur.weber@viacesi.fr>
2017-06-10 20:20:15 +0200

    Adds unit test

---
+++ SC2Bot.Tests/Helpers/Parser_Constructor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SC2Bot.Helpers;
+
+namespace SC2Bot.Tests.Helpers
+{
+    [TestClass]
+    public class Parser_Constructor
+    {
+        [TestMethod]
+        public void Should_Create_Parser()
+        {
+            Parser parser = new Parser();
+        }
+
+        [DataTestMethod]
+        [DataRow("!foo bar", "foo", new string[] { "bar" })]
+        [DataRow("!bar baz quz", "bar", new string[] { "baz", "quz" })]
+        [DataRow("!bar", "bar", null)]
+        [DataRow("!", null, null)]
+        public void Should_Create_Parser_With_Query(string query, string method, string[] parameters)
+        {
+            Parser parser = new Parser(query);
+            Assert.AreEqual(parser.GetQuery(), query);
+            Assert.AreEqual(parser.Method, method);
+            CollectionAssert.AreEqual(parser.Parameters, parameters);
+        }
+    }
+}
